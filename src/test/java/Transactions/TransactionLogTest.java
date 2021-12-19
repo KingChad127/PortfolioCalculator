2c886126e6b4d82cd0c3afdac4d81f356b08ba5a
KingChad127 <abhinavc@cs.utexas.edu>
2021-12-19 00:04:33 -0600

    Test searchByTicker method

--- src/test/java/Transactions/TransactionLogTest.java
+++ src/test/java/Transactions/TransactionLogTest.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransactionLogTest {
 
     @Test
-    public void testAdd() {
+    void testAddTransaction() {
         Transaction t1 = new Buy("AAPL", new BigDecimal("139.50"), 3, LocalDate.of(2021, 7, 20));
         Transaction t2 = new Sell("AAPL", new BigDecimal("145.70"), 1, LocalDate.of(2021, 7, 30));
         TransactionLog log = new TransactionLog();
@@ -23,6 +27,21 @@ class TransactionLogTest {
             assertEquals(hashcodes[i], t.hashCode());
             i++;
         }
+    }
+
+    @Test
+    void testSearchByTicker() {
+        Transaction t1 = new Buy("AAPL", new BigDecimal("139.50"), 3, LocalDate.of(2021, 7, 20));
+        Transaction t2 = new Buy("AAPL", new BigDecimal("140.50"), 3, LocalDate.of(2021, 7, 6));
+        Transaction t3 = new Sell("AAPL", new BigDecimal("142.33"), 6, LocalDate.of(2021, 8, 4));
+        Transaction t4 = new Sell("AAPL", new BigDecimal("145.70"), 1, LocalDate.of(2021, 10, 17));
+        TransactionLog log = new TransactionLog();
+        log.addTransaction(t1);
+        log.addTransaction(t2);
+        log.addTransaction(t3);
+        log.addTransaction(t4);
+        assertEquals(new TreeSet<>(Arrays.asList(t1, t2, t3, t4)),
+                log.searchByTicker("AAPL"));
     }
 
 }
