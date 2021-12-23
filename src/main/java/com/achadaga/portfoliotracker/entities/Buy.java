7723406b87c44a331d7286a9f28b2149e36ea25f
Abhinav Chadaga <abhinavc@cs.utexas.edu>
2021-12-22 23:18:38 -0600

    Move WIDTH constant to constants

--- src/main/java/com/achadaga/portfoliotracker/entities/Buy.java
+++ src/main/java/com/achadaga/portfoliotracker/entities/Buy.java
@@ -1,5 +1,6 @@
 package com.achadaga.portfoliotracker.entities;
 
+import com.achadaga.portfoliotracker.app.Constants;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
@@ -15,7 +16,7 @@ public class Buy extends Transaction {
     BigDecimal roundedPrice = getPrice().setScale(2, RoundingMode.HALF_UP);
     return super.toString() + "\n\tBUY " + getTicker() + "\n\tpurchase price: $" + String.format(
         "%.2f", roundedPrice) + "\n\tshares " + "bought: " + getQuantity() + "\n" + "-".repeat(
-        WIDTH);
+        Constants.WIDTH);
   }
 
 }
